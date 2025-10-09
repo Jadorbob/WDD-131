1c3072303d5471408bcc622c71e6fc171fbf130e
Jadorbob <ctrjadon@gmail.com>
2025-10-08 23:30:38 -0600

    X2

---
+++ ponder/ponderw4/responsive.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('change');
+  nav.classList.toggle('open');
+});
